8addd2cb06d69bd9a3c786a10e192c966b879eed
Florian <b.florian91@icloud.com>
2020-01-21 16:50:43 +0100

    protect front-end part on form's

--- views/ViewAccountEdit.php
+++ views/ViewAccountEdit.php
@@ -7,7 +7,7 @@ class ViewAccountEdit extends View
   { ?>
     <div class="container container-settings">
       <div class="row justify-content-center mb-4">
-        <button class="rounded font-poppins main-color text-white w-25" onclick="toggleDiv('editName'); checkUsername()">Edit Profile</button>
+        <button class="rounded font-poppins main-color text-white w-25" onclick="toggleDiv('editName')">Edit Profile</button>
       </div>
       <div class="row justify-content-center mb-4">
         <button class="rounded font-poppins main-color text-white w-25" onclick="toggleDiv('editPass')">Edit Password</button>
@@ -19,34 +19,40 @@ class ViewAccountEdit extends View
       <!-- username -->
       <div id="editName" class="mb-4" style="display: none;">
       <hr class="blue-light">
+      <form action="#" onsubmit="return checkUsername()" method="post">
         <div class="md-form">
           <input type="text" id="editUsername" class="form-control rounded" placeholder="username">
-          <button type="submit" id="btnSubmit" class="rounded w-100 main-color text-white">Submit</button>
+          <button type="submit" id="btnUser" class="rounded w-100 main-color" style="color:white;">Submit</button>
         </div>
+      </form>
       </div>
 
       <!-- password -->
       <div class="mb-4 mt-4" id="editPass" style="display:none">
-      <hr class="blue-light">
-        <div class="md-form">
-          <input type="password" id="actualPassword" class="form-control rounded" placeholder="old password">
-        </div>
-        <div class="md-form">
-          <input type="password" id="newPassword" class="form-control rounded" placeholder="new password">
-        </div>
-        <div class="md-form">
-          <input type="password" id="confirmPassword" class="form-control rounded" placeholder="confirm password">
-        </div>
-        <button type="submit" onclick="toggleDiv('editPass')" class="rounded w-100 main-color text-white">Submit</button>
+        <hr class="blue-light">
+        <form action="#" onsubmit="return checkPassword()" method="post">
+          <div class="md-form">
+            <input type="password" id="oldPassword" class="form-control rounded" placeholder="old password">
+          </div>
+          <div class="md-form">
+            <input type="password" id="newPassword" class="form-control rounded" placeholder="new password">
+          </div>
+          <div class="md-form">
+            <input type="password" id="confirmPassword" class="form-control rounded" placeholder="confirm password">
+          </div>
+          <button type="submit" id="btnPass" class="rounded w-100 main-color" style="color: white;">Submit</button>
+        </form>
       </div>
 
       <!-- email -->
       <div class="mb-4" id="editMail" style="display: none;">
-      <hr class="blue-light">
-        <div class="md-form">
-          <input type="email" id="editMail" class="form-control" placeholder="email">
-          <button type="submit" onclick="toggleDiv('editMail')" class="rounded w-100 main-color text-white">Submit</button>
-        </div>
+        <hr class="blue-light">
+        <form action="#" method="post">
+          <div class="md-form">
+            <input type="email" id="editEmail" class="form-control" placeholder="email">
+            <button type="submit" class="rounded w-100 main-color text-white">Submit</button>
+          </div>
+        </form>
       </div>
       <script src="/lib/javascript/toggle.js"></script>
       <script src="/lib/javascript/main.js"></script>
