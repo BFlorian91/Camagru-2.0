b824211bf9f50d51cc7f2a9267999d3a86779ded
Florian <b.florian91@icloud.com>
2020-02-14 15:31:23 +0100

    sql table creation OK

--- config/setup.php
+++ config/setup.php
@@ -1,10 +1,10 @@
 <?php
 
   $message = new Tools();
-  
+
+  // CREATE DATABASE 
+
   try {
-    // phpinfo();
-    // die();
     $db = new PDO('mysql:host=fl_mysql;port:8081', 'root', 'r@@t123X');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->exec("DROP DATABASE IF EXISTS camagru");
@@ -13,5 +13,68 @@
 
   } catch(PDOException $e) {
     echo  $message->error($e->getMessage());
-    // die();
+    die(-1);
+  }
+
+  // CREATE USERS TABLES 
+  try {
+    $db->exec("USE camagru");
+    $db->exec("CREATE TABLE `users` (
+      `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      `username` VARCHAR(50) NOT NULL,
+      `mail` VARCHAR(100) NOT NULL,
+      `password` VARCHAR(255) NOT NULL,
+      `token` VARCHAR(100) NOT NULL,
+      `verified` VARCHAR(1) NOT NULL DEFAULT '0'  
+    )");
+    echo $message->success("Users TABLE has been created!");
+  } catch (PDOException $e) {
+    echo $message->error($e->getMessage());
+    die(-1);
+  }
+
+  // CREATE TABLE GALLERY
+  try {
+    $db->exec("CREATE TABLE `gallery` (
+      `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      `img` VARCHAR(100) NOT NULL,
+      `userId` INT(12) NOT NULL,
+      FOREIGN KEY (userId) REFERENCES users(id)
+    )");
+    echo $message->success("Gallery TABLE has been created!");
+  } catch(PDOException $e) {
+    echo $message->error($e->getMessage());
+    die(-1);
+  }
+
+  // CREATE TABLE LIKES
+  try {
+    $db->exec("CREATE TABLE `like` (
+      `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      `userId` INT(12) NOT NULL,
+      `galleryId` INT(12) NOT NULL,
+      `liked` VARCHAR(1) NOT NULL DEFAULT '0',
+      FOREIGN KEY (userId) REFERENCES users(id),
+      FOREIGN KEY (galleryId) REFERENCES gallery(id)
+    )");
+    echo $message->success("Like TABLE has been created!");
+  } catch(PDOException $e) {
+    echo $message->error($e->getMessage());
+    die(-1);
+  }
+
+  // CREATE TABLE COMMENT
+  try {
+    $db->exec("CREATE TABLE `comment` (
+      `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      `userId` INT(12) NOT NULL,
+      `galleryId` INT(12) NOT NULL,
+      `comment` VARCHAR(255) NOT NULL,
+      FOREIGN KEY (userId) REFERENCES users(id),
+      FOREIGN KEY (galleryId) REFERENCES gallery(id)  
+    )");
+    echo $message->success("Comment TABLE has been created!");
+  } catch(PDOException $e) {
+    echo $message->error($e->getMessage());
+    die(-1);
   }
